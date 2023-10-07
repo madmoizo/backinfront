90beb8853a1072a1597b77db532dc38fb2a3df6d
madmoizo <matthieu.dambrune@gmail.com>
2023-10-07 04:29:32 +0000

    build: eslint config file changed to match the new format

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022
+    },
+    parserOptions: {
+      sourceType: "module"
+    },
+    rules: {
+      "no-multiple-empty-lines": "off",
+      "quote-props": [1, "consistent"]
+    }
+  }
+]
